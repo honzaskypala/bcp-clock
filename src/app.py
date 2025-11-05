122cb3f2952c78501539c1f804ba676357380f2b
honzaskypala <honza@honza.info>
2025-11-05 09:39:24 +0100

    configuration web server added

--- src/app.py
+++ src/app.py
@@ -4,57 +4,38 @@
 
 import asyncio
 import machine
-from time import sleep
+import time
 from tc001 import FrameBuffer
 from bcp import Event
-from config import config, load_config
+import config
 import network
 import ntptime
 import wifimgr
 import socket
 
 async def main():
+
     TIMER_INACTIVE = const(0)
-    TIMER_REFRESHDATA = const(1)
+    TIMER_REFRESH_DATA = const(1)
     TIMER_COUNTDOWN = const(2)
+    TIMER_HTTP_CONFIG = const(3)
+
     timer_state = [TIMER_INACTIVE] * 4
     timer = [machine.Timer(i) for i in range(4)]
+
+    # hardware timer assignment:
+    # timer[0] : Refresh data @ 1 minute interval
+    # timer[1] : Display countdown @ 1 sec interval
+    # timer[2] : unused atm
+    # timer[3] : config http server check update @ 1 sec interval and terminate after 5 minutes of inactivity
 
     DISPLAY_BOOTUP = const(0)
     DISPLAY_EVENTNAME = const(1)
     DISPLAY_ROUNDNUMBER = const(2)
     DISPLAY_COUNTDOWN = const(3)
+    DISPLAY_SCROLL_ONCE = const(4)
+
     display_state = DISPLAY_BOOTUP
-
-    global config
-    config = load_config()
-    fb = FrameBuffer()
-
-    # splash screen
-    fb.clear()
-    fb.text("BCPclock", 0, 1, font="f3x5")
-    fb.show()
-
-    # connect to WiFi
-    network.hostname(config["hostname"])
-    wlan = wifimgr.get_connection(fb=fb, ssid="BCP-clock", device="BCP Clock")
-    if wlan is None:
-        # TODO: display error message, wait 10 secs and reboot
-        return
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('', 80))
-        s.listen(5)
-    except OSError as e:
-        # if the code in try block above fails, then a web server from wifimgr.get_connection()
-        # remains open, which is known bug of ESP32. Reboot the hardware to close it.
-        machine.reset()
-    ntptime.settime()
-
-    # TODO: check if event is non empty, otherwise run config AP
-
-    event = Event(config["event"])
 
     def display_round(round, total):
         """ Display current round indicator """
@@ -82,12 +63,15 @@ async def main():
 
     def countdown_callback(t):
         """ Countdown timer callback — display remaining time """
+        nonlocal display_state
+        if display_state == DISPLAY_SCROLL_ONCE:
+            return
         if event.overview["status"]["started"] and not event.overview["status"]["ended"]:
             remaining = event.remaining_seconds()
             fb.clear()
-            if remaining <= config["red"]:
+            if remaining <= config.config["red"]:
                 c = (255, 0, 0)
-            elif remaining <= config["yellow"]:
+            elif remaining <= config.config["yellow"]:
                 c = (255, 255, 0)
             else:
                 c = (255, 255, 255)
@@ -112,12 +96,15 @@ async def main():
 
     async def refresh_data():
         """ Async refresh data """
+        nonlocal event
         try:
             await event.refresh()
         except Exception as e:
             return
         nonlocal display_state
-        if event.overview["status"]["ended"] or not event.overview["status"]["started"]:
+        if display_state == DISPLAY_SCROLL_ONCE:
+            return
+        elif event.overview["status"]["ended"] or not event.overview["status"]["started"]:
             # event either not started or already ended; display event name
             if display_state != DISPLAY_EVENTNAME:
                 if timer_state[1] == TIMER_COUNTDOWN:
@@ -142,7 +129,7 @@ async def main():
             if timer_state[1] != TIMER_COUNTDOWN:
                 if timer_state[1] != TIMER_INACTIVE:
                     timer[1].deinit()
-                timer[1].init(period=config["countdowninterval"] * 1000, mode=machine.Timer.PERIODIC, callback=countdown_callback)
+                timer[1].init(period=config.config["countdowninterval"] * 1000, mode=machine.Timer.PERIODIC, callback=countdown_callback)
                 timer_state[1] = TIMER_COUNTDOWN
                 display_state = DISPLAY_COUNTDOWN
 
@@ -154,10 +141,100 @@ async def main():
         except Exception as e:
             pass
 
+    def mid_button_pressed(pin):
+
+        def check_http_update(t):
+
+            async def config_update():
+                config.http_config_updated = False
+                # nonlocal event
+                # event = Event(config.config["event"])
+                # refresh_callback(None)   # This does not work, request is sent, but we never get response. Therefore, rather reboot the device.
+                await asyncio.sleep(1)     # Wait 1 sec, so the user gets http response before machine reboot
+                machine.reset()
+
+            nonlocal http_start_time
+            if config.http_config_updated:
+                try:
+                    loop = asyncio.get_event_loop()
+                    loop.create_task(config_update())
+                except Exception as e:
+                    pass
+            elif time.time() - http_start_time > 5 * 60:
+                # reset device after 5 minutes of config webserver open and no save
+                machine.reset()
+
+        async def config_splash():
+            nonlocal display_state
+            fb.clear()
+            fb.text("Config", 0, 2, font="f3x5")
+            fb.show()
+            await asyncio.sleep(5)
+            fb.clear()
+            fb.text(".".join(wlan.ifconfig()[0].split(".")[:2]), 0, 2, font="f3x5")
+            fb.show()
+            await asyncio.sleep(5)
+            fb.clear()
+            fb.text(".".join(wlan.ifconfig()[0].split(".")[-2:]), 0, 2, font="f3x5")
+            fb.show()
+            await asyncio.sleep(5)
+            display_state = DISPLAY_BOOTUP
+
+        http_start_time = time.time()
+        nonlocal display_state
+        if display_state != DISPLAY_SCROLL_ONCE:
+            display_state = DISPLAY_SCROLL_ONCE
+            try:
+                loop = asyncio.get_event_loop()
+                loop.create_task(config.http_server())
+                loop.create_task(config_splash())
+            except Exception as e:
+                pass
+        
+        timer[3].init(period=1000, mode=machine.Timer.PERIODIC, callback=check_http_update)
+        timer_state[3] = TIMER_HTTP_CONFIG
+
+    config.load_config()
+    fb = FrameBuffer()
+
+    # splash screen
+    fb.clear()
+    fb.text("BCPclock", 0, 1, font="f3x5")
+    fb.show()
+
+    # connect to WiFi
+    network.hostname(config.config["hostname"])
+    wlan = wifimgr.get_connection(fb=fb, ssid="BCP-clock", device="BCP Clock")
+    if wlan is None:
+        # TODO: display error message, wait 10 secs and reboot
+        return
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('', 80))
+        s.listen(5)
+        s.close()
+    except OSError as e:
+        # if the code in try block above fails, then a web server from wifimgr.get_connection()
+        # remains open, which is known bug of ESP32. Reboot the hardware to close it.
+        machine.reset()
+    ntptime.settime()
+
+    if config.config["event"] == "":
+        # event not configured yet, launch the config web
+        mid_button_pressed(None)
+        while True:
+            await asyncio.sleep(1)
+
+    event = Event(config.config["event"])
+
     # we will get the event data refreshed periodically
-    timer[0].init(period=config["refreshinterval"] * 1000, mode=machine.Timer.PERIODIC, callback=refresh_callback)
-    timer_state[0] = TIMER_REFRESHDATA
+    timer[0].init(period=config.config["refreshinterval"] * 1000, mode=machine.Timer.PERIODIC, callback=refresh_callback)
+    timer_state[0] = TIMER_REFRESH_DATA
     refresh_callback(None)  # initial data fetch
+
+    button_pin = machine.Pin(27, machine.Pin.IN, machine.Pin.PULL_UP)
+    button_pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=mid_button_pressed)
 
     while True:
         await asyncio.sleep(1)
