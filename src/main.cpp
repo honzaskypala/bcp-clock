bff693082712658ee93b50b6c3f97a0d27ca34cd
honzaskypala <honza@honza.info>
2025-11-14 19:56:44 +0100

    main functionality (countdown) implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,159 @@
 #include <config.h>
 #include <wifimgr.h>
 #include <bcpevent.h>
+#include <time.h>
+
+enum {
+    DISPLAY_BOOT,
+    DISPLAY_SCROLL_ONCE,
+    DISPLAY_EVENT_NAME,
+    DISPLAY_EVENT_ROUND,
+    DISPLAY_EVENT_COUNTDOWN
+} displayState = DISPLAY_BOOT;
+
+hw_timer_t *countdownTimer = nullptr;
+bool updateCountdown = false;
+
+void stopCountdownTimer() {
+    if (countdownTimer != nullptr) {
+        timerEnd(countdownTimer);
+        countdownTimer = nullptr;
+    }
+    updateCountdown = false;
+}
+
+void displayUpdate() {
+    if (displayState == DISPLAY_SCROLL_ONCE) {
+        return;
+    }
+    if (BCPEvent.getID() != "") {
+
+        if (BCPEvent.isEnded() || !BCPEvent.isStarted()) {
+            // event either not started or already ended
+            if (displayState != DISPLAY_EVENT_NAME) {
+                stopCountdownTimer();
+                FrameBuffer.stopTextScroll();
+                FrameBuffer.textScroll(BCPEvent.getName(), 2, "f3x5", CRGB::Green, CRGB::Black, 16, 16);
+                displayState = DISPLAY_EVENT_NAME;
+            }
+
+        } else if (BCPEvent.getTimerLength() <= 0) {
+            // no timer, show round only
+            if (displayState != DISPLAY_EVENT_ROUND) {
+                stopCountdownTimer();
+                FrameBuffer.stopTextScroll();
+                FrameBuffer.textCentered("Round " + String(BCPEvent.getCurrentRound()), 2, "f3x5", CRGB::White, true, true);
+                displayState = DISPLAY_EVENT_ROUND;
+            }
+
+        } else {
+            // show countdown
+            if (displayState != DISPLAY_EVENT_COUNTDOWN) {
+                FrameBuffer.stopTextScroll();
+                countdownTimer = timerBegin(3, 80, true);
+                timerAttachInterrupt(countdownTimer, []() { updateCountdown = true; }, true);
+                timerAlarmWrite(countdownTimer, 1000000, true);
+                timerAlarmEnable(countdownTimer);
+                displayState = DISPLAY_EVENT_COUNTDOWN;
+            }
+        }
+    }
+}
+
+void displayRound(int currentRound, int totalRounds) {
+    CRGB activeColor = CRGB::DeepSkyBlue,
+         inactiveColor = 0x0f0f0f;
+    const int line = 7;
+    FrameBuffer.hline(0, line, CFrameBuffer::WIDTH, CRGB::Black, false);
+    if (totalRounds < FrameBuffer.WIDTH / 2) {
+        int lineWidth = CFrameBuffer::WIDTH / totalRounds, offset;
+        switch (totalRounds) {
+            case 3:
+            case 5:
+            case 14:
+                offset = 2;
+                break;
+            case 7:
+            case 9:
+            case 13:
+                offset = 3;
+                break;
+            case 12:
+                offset = 4;
+                break;
+            case 11:
+                offset = 5;
+                break;
+            default:
+                offset = 1;
+        }
+        for (int i = 0; i < totalRounds; i++) {
+            FrameBuffer.hline(i * lineWidth + offset, line, lineWidth - 1, (i == currentRound - 1) ? activeColor : inactiveColor, false);
+        }
+    } else {
+        int t = totalRounds <= FrameBuffer.WIDTH ? totalRounds : FrameBuffer.WIDTH;
+        int start = (FrameBuffer.WIDTH - (t + (t <= FrameBuffer.WIDTH - 2 ? 2 : 0))) / 2;
+        for (int i = 0; i < t; i++) {
+            if (totalRounds <= FrameBuffer.WIDTH - 2 && (i == currentRound - 1 || i == currentRound)) {
+                start++;
+            }
+            FrameBuffer.pixel(start + i, line, (i == currentRound - 1) ? activeColor : inactiveColor, false);
+        }
+    }
+}
+
+void countdownUpdate() {
+    if (!updateCountdown) {
+        return;
+    }
+    updateCountdown = false;
+
+    long remaining = BCPEvent.getRoundEndEpoch() - time(nullptr);
+
+    CRGB color = CRGB::White;
+    if (remaining <= Config.getRedThreshold()) {
+        color = CRGB::Red;
+    } else if (remaining <= Config.getYellowThreshold()) {
+        color = CRGB::Yellow;
+    }
+
+    String font = "f4x6";
+    int x = remaining < 0 ? 0 : 5,
+        y = 0;
+    if (BCPEvent.getTimerLength() > 3600) {
+        font = "f3x5";
+        x = remaining < 0 ? -1 : 3;
+        y = 1;
+    }
+
+    int t = remaining < 0 ? -remaining : remaining;
+    int hours = t / 3600;
+    int minutes = (t % 3600) / 60;
+    int seconds = t % 60;
+
+    String out  = remaining < 0 ? "-" : "";;
+
+    if (BCPEvent.getTimerLength() <= 3600) {
+        // mm:ss
+        if (remaining < -3599) {
+            out = "-XX:XX";
+        } else {
+            out += String(minutes / 10) + String(minutes % 10) + ":" +
+                   String(seconds / 10) + String(seconds % 10);
+        }
+    } else {
+        // h:mm:ss
+        out += String(hours) + ":" +
+               String(minutes / 10) + String(minutes % 10) + ":" +
+               String(seconds / 10) + String(seconds % 10);
+    }
+
+    FrameBuffer.text(out, x, y, font, color, true, false);
+
+    displayRound(BCPEvent.getCurrentRound(), BCPEvent.getNumberOfRounds());
+
+    FrameBuffer.show();
+}   
 
 void displayRefresh(void* parameter) {
     while (true) {
@@ -12,6 +165,9 @@ void displayRefresh(void* parameter) {
         }
         if (FrameBuffer.doProgressStep()) {
             FrameBuffer.progressStep();
+        }
+        if (updateCountdown) {
+            countdownUpdate();
         }
         vTaskDelay(pdMS_TO_TICKS(10));
     }
@@ -44,21 +200,14 @@ void setup() {
     FrameBuffer.progressStart();
 
     WifiMgr.connect();
-
-    BCPEvent.setID("https://www.bestcoastpairings.com/event/IaDn1zzGAq5b");
-    Serial.println("Event ID: " + BCPEvent.getID());
+    BCPEvent.setID("https://www.bestcoastpairings.com/organize/event/HW0pjCBn5deR?active_tab=roster");
     BCPEvent.refreshData();
-    Serial.println("Event Name: " + BCPEvent.getName());
-    Serial.println("Event Started: " + String(BCPEvent.isStarted()));
-    Serial.println("Event Ended: " + String(BCPEvent.isEnded()));
-    Serial.println("Number of Rounds: " + String(BCPEvent.getNumberOfRounds()));
-    Serial.println("Current Round: " + String(BCPEvent.getCurrentRound()));
 
     FrameBuffer.progressStop();
-
-    FrameBuffer.textScroll(BCPEvent.getName(), 2, "f3x5", CRGB::Green, CRGB::Black, 16, 16);
 }
 
 void loop() {
+    displayUpdate();
     delay(60000);
+    BCPEvent.refreshData();
 }
